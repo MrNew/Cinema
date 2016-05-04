7521d2a29b678e0963faedd28841f3121f531085
Chen <1012030328.qq.com>
2016-05-04 10:19:49 +0800

    sdfsdf

---
+++ wet/wet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wet
+//
+//  Created by lanou on 16/5/4.
+//  Copyright © 2016年 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wet/wet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wet
+//
+//  Created by lanou on 16/5/4.
+//  Copyright © 2016年 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
